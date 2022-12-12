01ec0d34911db600e17de6083aee797a2ab8f35d
Kamil Malinowski <kamil.malinowski.mlu@gmail.com>
2022-12-12 17:23:26 +0100

    Fix import links

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-import { lazyImages } from "/js/_lazyImages.js";
-import { cookies } from "/js/_cookies.js";
-import { header } from "/js/_header.js";
+import { lazyImages } from "./js/_lazyImages.js";
+import { cookies } from "./js/_cookies.js";
+import { header } from "./js/_header.js";
 
 cookies();
 header();
